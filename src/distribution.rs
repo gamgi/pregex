00594de5e5ea1d463a94cc227e8145e4427a348a
Pietu R <pietu_r@gmx.com>
2022-01-22 16:06:49 +0200

    add binomial distribution

--- src/distribution.rs
+++ src/distribution.rs
@@ -3,7 +3,7 @@ use crate::ast::{AstNode, Kind};
 use crate::nfa::{State, StateParams};
 use itertools::Itertools;
 use pest;
-use statrs::distribution::{Discrete, Geometric};
+use statrs::distribution::{Binomial, Discrete, Geometric};
 use statrs::statistics::Distribution;
 use std::collections::{HashMap, HashSet};
 use std::fmt;
@@ -11,8 +11,9 @@ use std::fmt;
 #[derive(Debug, PartialEq, PartialOrd, Clone)]
 pub enum Dist {
     Constant(f64),        // p
-    ExactlyTimes(u64),    // n_min
+    ExactlyTimes(u64),    // n_match
     PGeometric(u64, f64), // n_min, p
+    PBinomial(u64, f64),  // n_max, p
 }
 
 impl fmt::Display for Dist {
@@ -21,6 +22,7 @@ impl fmt::Display for Dist {
             Dist::Constant(_) => write!(f, ""),
             Dist::ExactlyTimes(n) => write!(f, ""),
             Dist::PGeometric(_, p) => write!(f, "~Geo({})", p),
+            Dist::PBinomial(_, p) => write!(f, "~Bin({})", p),
         }
     }
 }
@@ -53,6 +55,10 @@ impl Dist {
                 let p: f64 = param.as_str().parse().unwrap();
                 Dist::PGeometric(n_min, p)
             }
+            "bin" => {
+                let std_dev: f64 = param.as_str().parse().unwrap();
+                Dist::PBinomial(n_min, std_dev)
+            }
             _ => {
                 panic!("Unknown distribution {}", name)
             }
@@ -65,22 +71,31 @@ pub fn evaluate(p0: f64, dist: &Option<Dist>, n: u64) -> (f64, f64) {
     if let Some(dist) = dist {
         return match dist {
             Dist::Constant(p) => (p0 * p, p0 * p),
-            Dist::ExactlyTimes(match_n) => {
-                if n == *match_n {
+            Dist::ExactlyTimes(n_match) => {
+                if n == *n_match {
                     (0.0, p0)
-                } else if n < *match_n {
+                } else if n < *n_match {
                     (p0, 0.0)
                 } else {
                     (0.0, 0.0)
                 }
             }
-            Dist::PGeometric(match_n, c) => {
-                if n < *match_n {
+            Dist::PGeometric(n_min, c) => {
+                if n < *n_min {
                     return (p0, 0.0);
                 }
-                let x = n - match_n + 1;
+                let x = n - n_min + 1;
                 let p = Geometric::new(*c).unwrap().pmf(x);
                 (p * p0, (1.0 - p) * p0)
+            }
+            Dist::PBinomial(n_max, p) => {
+                if n > *n_max {
+                    return (p0, 0.0);
+                    // return (0.0, 0.0);
+                }
+                let x = n;
+                let p = Binomial::new(*p, *n_max).unwrap().pmf(x);
+                ((1.0 - p) * p0, p * p0)
             }
         };
     }
@@ -166,5 +181,33 @@ mod test {
             evaluate(0.5, &Some(Dist::PGeometric(2, 0.5)), 2),
             (0.25, 0.25)
         );
+    }
+    #[test]
+    fn test_distribution_binomial_degenerate() {
+        // If p = 1 the distribution is concentrated at n
+        assert_eq!(evaluate(1.0, &Some(Dist::PBinomial(0, 1.0)), 0), (0.0, 1.0));
+        assert_eq!(evaluate(1.0, &Some(Dist::PBinomial(0, 1.0)), 1), (1.0, 0.0));
+        assert_eq!(evaluate(1.0, &Some(Dist::PBinomial(1, 1.0)), 1), (0.0, 1.0));
+    }
+
+    #[test]
+    fn test_distribution_binomial_up_to_1() {
+        assert_eq!(evaluate(1.0, &Some(Dist::PBinomial(1, 0.5)), 0), (0.5, 0.5));
+        assert_eq!(evaluate(1.0, &Some(Dist::PBinomial(1, 0.5)), 1), (0.5, 0.5));
+        assert_eq!(evaluate(1.0, &Some(Dist::PBinomial(1, 0.5)), 2), (1.0, 0.0));
+    }
+
+    #[test]
+    fn test_distribution_binomial_up_to_2() {
+        assert_eq!(
+            evaluate(1.0, &Some(Dist::PBinomial(2, 0.5)), 0),
+            (0.75, 0.25)
+        );
+        assert_eq!(evaluate(1.0, &Some(Dist::PBinomial(2, 0.5)), 1), (0.5, 0.5));
+        assert_eq!(
+            evaluate(1.0, &Some(Dist::PBinomial(2, 0.5)), 2),
+            (0.75, 0.25)
+        );
+        assert_eq!(evaluate(1.0, &Some(Dist::PBinomial(2, 0.5)), 3), (1.0, 0.0));
     }
 }
